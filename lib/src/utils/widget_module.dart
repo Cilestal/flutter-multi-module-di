eba099b7d8e83ef1c800d81003df9928b9dd593d
Michael Lang <michaelcilestal@gmail.com>
2021-03-12 12:38:12 +0200

    minor fix

--- lib/src/utils/widget_module.dart
+++ lib/src/utils/widget_module.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_multi_module_di/flutter_multi_module_di.dart';
+part of 'package:flutter_multi_module_di/flutter_multi_module_di.dart';
 
 abstract class WidgetModule implements Module {
   final BuildContext _context;
